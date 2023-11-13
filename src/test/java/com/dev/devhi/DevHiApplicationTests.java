9a51ceb0e34e675c67fc6581345caebc4dbd2ab8
hj-k66 <hjx66@naver.com>
2023-11-13 17:47:15 +0900

    init: init project

---
+++ src/test/java/com/dev/devhi/DevHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.devhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
